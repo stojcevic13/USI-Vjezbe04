b0d5f9221080a792024f5e50ba0d5ff4117fc942
unknown <99722952+stojcevic13@users.noreply.github.com>
2022-04-18 13:10:48 +0200

    validation testiranje

--- Kalkulator2/Program.cs
+++ Kalkulator2/Program.cs
@@ -16,14 +16,37 @@ namespace Kalkulator2
 
         static void StartCalculator()
         {
+            string[] input;
+            do
+            {
+
+                input = inputExpression();
+                if (input.Length != 3)
+                    break;
+
+                double num1 = Convert.ToDouble(input[0]);
+                char oper = Convert.ToChar(input[1]);
+                double num2 = Convert.ToDouble(input[2]);
+
+
+                Console.WriteLine("Vas unos: " + input[0] + input[1] + input[2]);
+            } while (true);
+        }
+
+
+        static string[] inputExpression()
+        {
             string input;
             do
             {
                 Console.Write("Unesite izraz: ");
                 input = Console.ReadLine().ToLower();
                 if (input.Equals("exit") || input.Equals("kraj"))
-                    break;
-                Console.WriteLine("Vas unos: " + input);
+                    return new string[1];
+                string[] tokens = input.Trim().Split();
+                if (tokens.Length == 3)
+                    return tokens;
+                Console.WriteLine("Pogresan unos! Pokusajte ponovo.\n");
             } while (true);
         }
     }
